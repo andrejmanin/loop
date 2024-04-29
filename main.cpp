28bc1bfa62072bc50a2c657dda01956388e322fa
rocert92 <rocert92@gmail.com>
2024-04-29 20:23:43 +0300

    Init commit

--- main.cpp
+++ main.cpp
@@ -4,66 +4,59 @@
 using namespace std;
 
 void task_1() {
-	int number, i = 0;
+	int number, sum = 0;
+	cout << "Enter number from 0 to 500: ";
 	cin >> number;
-	while (i != number)
+	while (number <= 500)
 	{
-		cout << i << endl;
-		i++;
-	}
-}
-
-
-void task_2() {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	int a, b, i;
-	cin >> a >> b;
-	i = (a > b) ? b : a;
-	int max = ((a < b) ? b : a);
-	while (i <= max) {
-		if (i % 2 == 0) {
-			SetConsoleTextAttribute(hConsole, 2);
-			cout << i << endl;
-			SetConsoleTextAttribute(hConsole, 7);
-		}
-		if (i % 7 == 0) {
-			SetConsoleTextAttribute(hConsole, 6);
-			cout << i << endl;
-			SetConsoleTextAttribute(hConsole, 7);
-		}
-		if (i % 2 != 0) {
-			SetConsoleTextAttribute(hConsole, 4);
-			cout << i << endl;
-			SetConsoleTextAttribute(hConsole, 7);
-		}
-		i++;
-	}
-}
-
-void task_3() {
-	int a, b, i, sum = 0;
-	cin >> a >> b;
-	i = (a > b) ? b : a;
-	int max = ((a < b) ? b : a);
-	while (i <= max)
-	{
-		sum += i;
-		i++;
-	}
-}
-
-void task_4() {
-	int num;
-	long long sum = 0;
-	cin >> num;
-	while (num != 0) {
-		cin >> num;
-		sum += num;
+		sum += number;
+		number++;
 	}
 	cout << "Sum: " << sum << endl;
 }
 
+void task_2() {
+	int x, y;
+	cin >> x >> y;
+	cout << "x ^ y = " << pow(x, y) << endl;
+}
+
+void task_3() {
+	int sum = 0, i = 1;
+	while (i <= 1000) {
+		sum += i;
+		i++;
+	}
+	cout << "Arithmetic mean of numbers from 1 to 1000 is: " << sum / 1000 << endl;
+}
+
+void task_4() {
+	int a;
+	do
+	{
+		cout << "Enter a number from 1 to 20: ";
+		cin >> a;
+	} while (a < 1 || a > 20);
+	int dob = 1, i = a;
+	while (i <= 20)
+	{
+		dob *= i;
+		i++;
+	}
+	cout << "Dob from a to 20 is: " << dob << endl;
+}
+
+void task_5() {
+	int number, i = 1;
+	cout << "Enter a number: ";
+	cin >> number;
+	while (i <= 10) {
+		cout << number << " * " << i << " = " << number * i << endl;
+		i++;
+	}
+}
+
 int main() {
-	task_4();
+	task_5();
 	return 0;
 }
